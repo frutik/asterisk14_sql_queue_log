0d6c9a7ead35bd3d8679320b5c121adb29e9a753
Andrew Kornilov <frutik@gmail.com>
2012-02-02 20:15:24 +0200

    initial commit

---
+++ queue_log2sql_insert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#LOG='/var/log/asterisk/queue_log'
+LOG=${1}
+
+inotifywait -mq -e modify ${LOG} | while read file; do tail -1 ${LOG} | awk -vd="'" -F '|' \
+    '{printf("insert into queue_log (time, callid, queuename, agent, event, data) values (%s%s%s, %s%s%s, %s%s%s, %s%s%s, %s%s%s", d,$1,d,d,$2,d,d,$3,d,d,$4,d,d,$5,d)} END {printf(", %s", d)} END { for (i = 6; i <= NF; i++) printf("%s%s", FS,$i)} END {print d} END {print ");"}'; done

b322dc1ed0aea3506a08b3dc7f62d57226ad3d79
Andrew Kornilov <frutik@gmail.com>
2012-02-09 22:39:43 +0200

    -

--- queue_log.sh
+++ queue_log.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-LOCKFILE='/var/lock/queue_log.lock'
+#LOCKFILE='/var/lock/queue_log.lock'
 
-if [ ! -e $LOCKFILE ]; then
-   trap "rm -f $LOCKFILE; exit" INT TERM EXIT
-   touch $LOCKFILE
+#if [ ! -e $LOCKFILE ]; then
+#   trap "rm -f $LOCKFILE; exit" INT TERM EXIT
+#   touch $LOCKFILE
 
    /opt/asterisk14_sql_queue_log/queue_log2sql_insert.sh /var/log/asterisk/queue_log 2>/dev/null | psql -U postgres asterisk
 
-   rm $LOCKFILE
-   trap - INT TERM EXIT
-else
-   echo "already running"
-fi
+#   rm $LOCKFILE
+#   trap - INT TERM EXIT
+#else
+#   echo "already running"
+#fi
 
